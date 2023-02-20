4aee990720f98dd2f9efde12c4fbdc1e03f47d13
Lapide <64395933+nonumbershere@users.noreply.github.com>
2023-02-20 10:34:18 -0500

    Create crimsonweb_pack.js

---
+++ scripts/crimsonweb_pack.js
@@ -0,0 +1,26 @@
+// Made for 0.1.9 and above. (By Lapide)
+
+if(itemExists(`Skeleton Knife | Crimson Web`)){client.user.inventory.push(`Skeleton Knife | Crimson Web`)}
+if(itemExists(`Paracord Knife | Crimson Web`)){client.user.inventory.push(`Paracord Knife | Crimson Web`)}
+if(itemExists(`Nomad Knife | Crimson Web`)){client.user.inventory.push(`Nomad Knife | Crimson Web`)}
+if(itemExists(`Survival Knife | Crimson Web`)){client.user.inventory.push(`Survival Knife | Crimson Web`)}
+if(itemExists(`Classic Knife | Crimson Web`)){client.user.inventory.push(`Classic Knife | Crimson Web`)}
+if(itemExists(`Talon Knife | Crimson Web`)){client.user.inventory.push(`Talon Knife | Crimson Web`)}
+if(itemExists(`Stiletto Knife | Crimson Web`)){client.user.inventory.push(`Stiletto Knife | Crimson Web`)}
+if(itemExists(`Ursus Knife | Crimson Web`)){client.user.inventory.push(`Ursus Knife | Crimson Web`)}
+if(itemExists(`Navaja Knife | Crimson Web`)){client.user.inventory.push(`Navaja Knife | Crimson Web`)}
+if(itemExists(`Bowie Knife | Crimson Web`)){client.user.inventory.push(`Bowie Knife | Crimson Web`)}
+if(itemExists(`Shadow Daggers | Crimson Web`)){client.user.inventory.push(`Shadow Daggers | Crimson Web`)}
+if(itemExists(`Falchion Knife | Crimson Web`)){client.user.inventory.push(`Falchion Knife | Crimson Web`)}
+if(itemExists(`Butterfly Knife | Crimson Web`)){client.user.inventory.push(`Butterfly Knife | Crimson Web`)}
+if(itemExists(`Huntsman Knife | Crimson Web`)){client.user.inventory.push(`Huntsman Knife | Crimson Web`)}
+if(itemExists(`Karambit | Crimson Web`)){client.user.inventory.push(`Karambit | Crimson Web`)}
+if(itemExists(`M9 Bayonet | Crimson Web`)){client.user.inventory.push(`M9 Bayonet | Crimson Web`)}
+if(itemExists(`Bayonet | Crimson Web`)){client.user.inventory.push(`Bayonet | Crimson Web`)}
+if(itemExists(`Flip Knife | Crimson Web`)){client.user.inventory.push(`Flip Knife | Crimson Web`)}
+if(itemExists(`Gut Knife | Crimson Web`)){client.user.inventory.push(`Gut Knife | Crimson Web`)}
+if(itemExists(`Desert Eagle | Crimson Web`)){client.user.inventory.push(`Desert Eagle | Crimson Web`)}
+if(itemExists(`R8 Revolver | Crimson Web`)){client.user.inventory.push(`R8 Revolver | Crimson Web`)}
+if(itemExists(`CZ75-Auto | Crimson Web`)){client.user.inventory.push(`CZ75-Auto | Crimson Web`)}
+if(itemExists(`SCAR-20 | Crimson Web`)){client.user.inventory.push(`SCAR-20 | Crimson Web`)}
+alerts.floatDialog('Crismon Web pack', 'Succesfully gave all 22 crimson web items')
